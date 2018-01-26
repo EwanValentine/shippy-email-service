054f1f4b3a9d8e40a6656908eb5ef6468f6537fa
Ewan Valentine <ewan.valentine89@gmail.com>
2018-01-26 09:34:42 +0000

    UPDATED

--- Makefile
+++ Makefile
@@ -6,6 +6,4 @@ run:
 		-p 50054 \
 		-e MICRO_SERVER_ADDRESS=:50054 \
 		-e MICRO_REGISTRY=mdns \
-		-e MICRO_BROKER=nats \
-		-e MICRO_BROKER_ADDRESS=0.0.0.0:4222 \
 		email-service
--- main.go
+++ main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"log"
 
 	pb "github.com/EwanValentine/shippy-user-service/proto/user"
 	micro "github.com/micro/go-micro"
-	"github.com/micro/go-micro/broker"
-	_ "github.com/micro/go-plugins/broker/nats"
 )
 
 const topic = "user.created"
+
+type Subscriber struct{}
+
+func (sub *Subscriber) Process(ctx context.Context, user *pb.User) error {
+	log.Println("Picked up a new message")
+	log.Println("Sending email to:", user.Name)
+	return nil
+}
 
 func main() {
 	srv := micro.NewService(
@@ -20,34 +26,10 @@ func main() {
 
 	srv.Init()
 
-	// Get the broker instance using our environment variables
-	pubsub := srv.Server().Options().Broker
-	if err := pubsub.Connect(); err != nil {
-		log.Fatal(err)
-	}
-
-	// Subscribe to messages on the broker
-	_, err := pubsub.Subscribe(topic, func(p broker.Publication) error {
-		var user *pb.User
-		if err := json.Unmarshal(p.Message().Body, &user); err != nil {
-			return err
-		}
-		log.Println(user)
-		go sendEmail(user)
-		return nil
-	})
-
-	if err != nil {
-		log.Println(err)
-	}
+	micro.RegisterSubscriber(topic, srv.Server(), new(Subscriber))
 
 	// Run the server
 	if err := srv.Run(); err != nil {
 		log.Println(err)
 	}
-}
-
-func sendEmail(user *pb.User) error {
-	log.Println("Sending email to:", user.Name)
-	return nil
 }
